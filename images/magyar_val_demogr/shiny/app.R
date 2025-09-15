861f341f84faf4b284dff3c3fbf3f40f4120e55c
mbkoltai <kolmisi@gmail.com>
2025-09-15 01:43:23 +0100

    images/magyar_val_demogr/ uj folder

--- images/magyar_val_demogr/shiny/app.R
+++ images/magyar_val_demogr/shiny/app.R
@@ -4,42 +4,81 @@ library(shiny); library(ggplot2); library(ggh4x); library(dplyr)
 l_plot <- readRDS("l_plot_21kut_median.RDS")
 
 ui <- fluidPage(
+  
+  tags$head(
+    tags$style(HTML("
+      body, .shiny-text-output, .shiny-input-container {
+        font-family: Calibri, Arial, sans-serif;
+      }
+    "))
+  ),
+  
+  tags$div(
+  style = "border: 2px solid #ccc; border-radius: 10px; padding: 10px;
+           width: 90%; margin: 10px auto; text-align: center; background-color: #f9f9f9;",
+  tags$h1(
+    style = "font-family: Calibri; font-size: 30px;",  # optional: adjust font size
+    HTML("2025 magyarországi közvéleménykutatások rávetítése az országos demográfiára:<br>
+      teljes népesség | nem | kor | végzettség | településméret")
+  )
+), 
+  
   tags$div(
   style = "border: 3px solid #ccc; padding: 10px 10px 0px 10px; margin-top: 10px; 
     border-radius: 10px; width: 700px; font-size: 18px;",
   selectInput("dataset", "ADATFORRÁS:", 
               choices = c("21 Kutatóközpont (2025/[04/06/08])" = "21_kut",
-                          "MEDIÁN (2025/08)" = "median")) ),
+                          "MEDIÁN (2025/08)" = "median"),
+            width = "450px" ) ),
+   # new input: label type
+  wellPanel(
+  style="border: 3px solid #ccc; padding: 10px 10px 10px 10px; margin-top: 10px; font-size: 16px; width: 60%;",  
+  radioButtons("label_type", "Címkék típusa:",
+               choices = c("abszolút szám"="abs", "százalék" = "pct"),
+               inline = T) ),
   # text
-  div(
-      style="border: 3px solid #ccc; padding: 10px 10px 10px 10px; margin-top: 10px; 
-              font-size: 16px;",  # növeli a szöveg méretét
+  tags$div(
   HTML("Válassz egy adatforrást a fenti menüből, és az ábra automatikusan frissül. <br> <br>
       MEGJEGYZÉSEK</b>: Az ábrák a 21kutatóintézet és a Medián 2025 nyári 
-      méréseit integrálják a KSH demográfiai adataival. <br> 
+      méréseit integrálják a KSH demográfiai adataival. 
+      Minden demográfiai adat a választásra jogosult, tehát 18 éves vagy idősebb, népességre vonatkozik, 
+      ami a KSH 2025-ös becslése szerint kb. 7.88 millió ember. <br> 
       A függőleges tengelyen a választók száma ezerben van megadva. <br>
-      <b>FONTOS</b>: Még ha a felmérések nagyon pontosak is lennének, a hibahatár így alkategóriánként nézve szinte
-      biztosan nagyobb, mint a teljes mintára nézve. <br> 
-      Tehát az oszlopok fölötti számokat, amelyek (ha abszolút számként kérjük őket) tízezerrel kerekítve lettek, 
+      <b>FONTOS</b>: Még ha a felmérések nagyon pontosak is (lennének), a hibahatár így, alkategóriánként nézve, szinte
+      biztosan nagyobb, mint a teljes mintára nézve. <br>
+      Tehát az oszlopok fölötti számokat, amelyek (ha abszolút számként kérjük őket) tízezerre kerekítve lettek, 
       nem érdemes pontos becsléseknek venni. Ehelyett csak azt mutatják meg, 
       hogy hány (százalék) szavazó lenne az adott kategóriában, 
       <b>ha</b> a mérés tökéletesen pontos lenne - ami szinte biztosan nem igaz. <br>
       Ugyanakkor annyit megmutatnak, hogy az egyes demográfiai csoportokban 
       (pl. szakmunkás végzettségűek, 65 év felettiek stb.) összesen mennyien vannak, 
-      illetve, ha a mérések hozzávetőlegesen pontosak, akkor azt is, hogy körülbelül hogyan oszlik el a 
-      népesség pártszimpátia szerint. <br>
-      Bizonyos demográfiai adatoknál (mint pl. a 74 éven felüliek végzettség szerinti eloszlása) 
-      szintén feltételezésekkel kellett élnem, mivel nem teljesek a KSH-nál megtalálható adatok. <br>
-      Erről további információ a projekt github-ján érhető el, az ábrákhoz használt csv táblázatokkal együtt: 
-      "), 
-    style = "width: 80%; margin-bottom: 30px;"),
-   # new input: label type
-  wellPanel(
-  style="border: 3px solid #ccc; padding: 10px 10px 10px 10px; margin-top: 10px;width: 60%;",  
-  radioButtons("label_type", "Címkék típusa:",
-               choices = c("Abszolút szám"="abs", "Százalék" = "pct"),
-               inline = T) ),
-  
+      illetve, <i>ha</i> a mérések hozzávetőlegesen pontosak, akkor azt is, hogy körülbelül hogyan oszlik el a 
+      népesség pártszimpátia szerint az adott kategóriákban. 
+      Az utóbbi (ti. a kategóriákon belüli pártpreferencia eloszlás) valószínűleg jelentős(ebb) hibákat tartalmaz. <br>
+      A 74 éven felüliek végzettség szerinti eloszlásánál feltételezésekkel kellett élnem, 
+      mivel nem találtam erre vonatkozó adatot a KSH-nál. 
+      Emellett, a KSH felnőtt népesség adata kb. 120 ezerrel nagyobb, mint a 2022-es települési választási adatokból 
+      kivonható adat. Ennek nem vagyok benne biztos, hogy mi az oka (talán a nem bejelentett külföldön élők). 
+      Erről további információ a 
+      <a href='https://github.com/mbkoltai/mbkoltai.github.io/blob/source/images/magyar_val_demogr/script.R' 
+      target='_blank'>projekt github-ján</a> érhető el, az ábrákhoz használt csv táblázatokkal együtt.
+      Ez a két demográfiai pontatlanság nem változtat sokat (valószínűleg kb. néhány tízezret) 
+      a végzettség és településtípus szerinti demográfiai csoportok méretén. <br>
+      A MEDIÁN 2002-es választások óta adott választás előtti - az eredményekkel összehasonlított - előrejelzéseit 
+      <a href='https://docs.google.com/spreadsheets/d/1NUEgN7eV7MoZqi8dd9-xAzWxY_u2u_HYImV6l_U5qDE/' 
+      target='_blank'>itt gyűjtöttem össze</a>. <br>
+      A 21kutközpont 2024-es 
+      <a href='https://mbkoltai.com/ep2024-hungary-datavis/' target='_blank'>EP-választások</a>
+      előtti (gyakorlatilag tökéletesre sikerült) 
+      <a href='https://24.hu/belfold/2024/06/09/ep-valasztas-2024-exit-poll-mandatumbecsles/' 
+      target='_blank'> előrejelzése itt</a>. <br>
+      A többi közvéleménykutató <i> record</i>-ja annyira zűrös, hogy inkább nem használtam őket, 
+      illetve demográfiai lebontást általában nem is csinálnak."
+      ), 
+      style = "border: 3px solid #ccc; padding: 10px; margin-top: 10px; 
+             font-size: 16px; width: 80%; margin-left: auto; 
+              margin-right: auto; margin-bottom: 30px;"    ),
+  # MAIN PLOT
   plotOutput("mainplot", width="96%", height = "800px")
 )
 
