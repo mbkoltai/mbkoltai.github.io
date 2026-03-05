c621ed9658f0c8eded49e6aa726a43333872a994
mbkoltai <kolmisi@gmail.com>
2026-03-05 13:50:45 +0000

    valaszt demogr update

--- images/magyar_val_demogr2025/functions.R
+++ images/magyar_val_demogr2025/functions.R
@@ -32,8 +32,6 @@ l_plot$standard_theme <- theme( # for publ plots
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 
-
-
 convert_hu_date <- function(x) {
   months_hu <- c(
   január="01", február="02", március="03", április="04",
@@ -41,4 +39,109 @@ convert_hu_date <- function(x) {
   szeptember="09", október="10", november="11", december="12" )
   m <- str_match(x, "([0-9]{4})\\.\\s*([[:alpha:]]+)")[,2:3]
   sprintf("%s/%s", m[,1], months_hu[m[,2]])
+}
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+# ---- helpers -----------------------------------------------------------
+
+median_demog_file <- function(period) {
+  # e.g. "2025_06" -> "input_files/median_2025_06_nem_vegzettseg_telepules_vegzettseg.csv"
+  paste0("input_files/median_", period, "_nem_vegzettseg_telepules_vegzettseg.csv")
+}
+
+median_fullpop_file <- function(period) {
+  # e.g. "2025_06" -> "input_files/median_2025_06_teljes_nepesseg.csv"
+  paste0("input_files/median_", period, "_teljes_nepesseg.csv")
+}
+
+load_median_demog <- function(period) {
+  file <- median_demog_file(period)
+  
+  if (period == "2025_06") {
+    # special structure
+    read_csv(file) %>%
+      select(!url) %>%
+      pivot_longer(!c(kateg_tipus, kateg, datum), names_to = "part") %>%
+      mutate(
+        arány = value / 100,
+        part  = gsub("\\*", "", part),
+        kateg = tolower(kateg),
+        kateg = gsub(
+          "/vagy idősebb", "\\+",
+          gsub(" éves", "",
+          gsub(" végzettség", "",
+          gsub("\\, ", "/", kateg)))
+        )
+      ) %>%
+      select(!value) %>%
+      group_by(kateg_tipus) %>%
+      { set_names(group_split(.,.keep = FALSE), group_keys(.)$kateg_tipus) } %>%
+      as.list()
+    
+  } else if (period == "2025_08") {
+    # 2025_08 layout
+    read_csv(file) %>%
+      rename(tipus = filter) %>%
+      pivot_longer(!c(tipus, kateg), names_to = "part") %>%
+      mutate(
+        arány = value / 100,
+        kateg = tolower(kateg),
+        datum = gsub("_", "/", period)
+      ) %>%
+      select(!value) %>%
+      group_by(tipus) %>%
+      { set_names(group_split(.,.keep = FALSE), group_keys(.)$tipus) } %>%
+      as.list()
+    
+  } else {
+    # 2025_11, 2026_01 etc. (your existing loop pattern)
+    read_csv(file) %>%
+      rename(tipus = filter) %>%
+      pivot_longer(!c(tipus, kateg, url), names_to = "part") %>%
+      mutate(
+        arány = value / 100,
+        kateg = tolower(kateg),
+        datum = gsub("_", "/", period)
+      ) %>%
+      select(!c(value, url)) %>%
+      group_by(tipus) %>%
+      { set_names(group_split(.,.keep=F), group_keys(.)$tipus) } %>%
+      as.list()
+  }
+}
+
+load_median_fullpop <- function(period) {
+  file <- median_fullpop_file(period)
+  
+  if (period == "2025_06") {
+    read_csv(file) %>%
+      rename(part = Párt) %>%
+      select(part, `teljes népesség`) %>%
+      pivot_longer(!part, names_to = "kateg") %>%
+      mutate(
+        arány = value / 100,
+        datum = "2025/06"
+      ) %>%
+      select(!value)
+    
+  } else {
+    read_csv(file) %>%
+      pivot_longer(!c(kateg, url, datum), names_to = "part") %>%
+      filter(grepl("Teljes", kateg)) %>%
+      mutate(
+        kateg = tolower(kateg),
+        arány = value / 100
+      ) %>%
+      select(!c(value, url)) %>%
+      relocate(datum,.after = last_col()) %>%
+      bind_rows(
+        data.frame(
+          kateg = "teljes népesség",
+          part  = "Jobbik",
+          datum = gsub("_", "/", period)
+          # arány assumed 0 as in comment
+        )
+      )
+  }
 }
--- images/magyar_val_demogr2025/script_magyar_val_demogr.R
+++ images/magyar_val_demogr2025/script_magyar_val_demogr.R
@@ -41,109 +41,34 @@ l_part_data$`21_kut`$telep_tipus <- read_delim(
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
-# MEDIAN 2025/06, tobb demogr kateg
+# Median adatok betoltese 
 
-# MEDIAN 2025/06, tobb demogr kateg
-# https://datawrapper.dwcdn.net/Ns8XR/10/
-l_part_data[["median"]][["2025_06"]] = list()
-l_part_data[["median"]][["2025_06"]] <- read_csv(
-  "input_files/median_2025_06_nem_vegzettseg_telepules_vegzettseg.csv") %>%
-  select(!url) %>%
-  pivot_longer(!c(kateg_tipus,kateg,datum),names_to="part") %>%
-  mutate(arány=value/100,
-        part=gsub("\\*","",part),
-        kateg=tolower(kateg),
-        kateg=gsub("/vagy idősebb","\\+",gsub(" éves","",gsub(" végzettség","",gsub("\\, ","/",
-                kateg))))  ) %>% 
-  select(!value) %>%
-  group_by(kateg_tipus) %>% { 
-    set_names(group_split(.,.keep=F), group_keys(.)$kateg_tipus) } %>%
-  as.list()
+periods <- c("2025_06", "2025_08", "2025_11", "2026_01", "2026_02")
 
-# l_part_data[["median"]][["2025_06"]]$KORCSOPORT
-# l_part_data[["median"]][["2025_08"]]$ÉLETKOR
-names(l_part_data[["median"]][["2025_06"]])[
-  names(l_part_data[["median"]][["2025_06"]]) %in% "KORCSOPORT"] <- "ÉLETKOR"
-
-# teljes nepesseg
-# datawrapper.dwcdn.net/RLw52/2/
-l_part_data[["median"]][["2025_06"]]$telj_nepesseg_partok <- read_csv(
-  "input_files/median_2025_06_teljes_nepesseg.csv") %>% 
-  rename(part=Párt) %>%
-  select(part,`teljes népesség`) %>%
-  pivot_longer(!part,names_to = "kateg") %>%
-  mutate(arány=value/100,datum="2025/06") %>%
-  select(!value)
-
-l_part_data$median$`2025_06`$TELEPÜLÉS <- l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS
-l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS=NULL
-
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
-# MEDIAN 2025/08, tobb demogr kateg
-# source: https://flo.uri.sh/visualisation/25049528/embed?auto=1
-
-l_part_data[["median"]][["2025_08"]] <- list()
-l_part_data[["median"]][["2025_08"]] <- read_csv(
-  "input_files/median_2025_08_nem_vegzettseg_telepules_vegzettseg.csv") %>%
-  rename(tipus=filter) %>%
-  pivot_longer(!c(tipus,kateg),names_to="part") %>%
-  mutate(arány=value/100,
-         kateg=tolower(kateg),
-         datum="2025/08"  ) %>% 
-  select(!value) %>%
-  group_by(tipus) %>% { set_names(group_split(.,.keep=F), group_keys(.)$tipus) } %>%
-  as.list()
-
-# teljes nepesseg partokra bontva
-# https://flo.uri.sh/visualisation/25049245/embed?auto=1
-l_part_data[["median"]][["2025_08"]]$telj_nepesseg_partok <- read_csv(
-  "input_files/median_2025_08_teljes_nepesseg.csv") %>% 
-  pivot_longer(!c(kateg,url,datum),names_to="part") %>%
-  filter(grepl("Teljes",kateg)) %>%
-  mutate(kateg=tolower(kateg),arány=value/100) %>%
-  select(!c(value,url)) %>%
-  relocate(c(datum),.after=last_col()) %>%
-  bind_rows(data.frame(kateg="teljes népesség",
-    part="Jobbik",datum="2025/08")) # arány=0,
-
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
-# MEDIAN 2025/11, tobb demogr kateg
-# source: https://flo.uri.sh/visualisation/26553099/embed?auto=1
-
-# MEDIAN 2025/11, tobb demogr kateg
-# source: https://flo.uri.sh/visualisation/26553099/embed?auto=1
-
-for (k_yr in c("2025_11","2026_01")) {
-l_part_data[["median"]][[k_yr]] <- list()
-l_part_data[["median"]][[k_yr]] <- read_csv(
-  paste0("input_files/median_",k_yr,"_nem_vegzettseg_telepules_vegzettseg.csv")) %>%
-  rename(tipus=filter) %>%
-  pivot_longer(!c(tipus,kateg,url),names_to="part") %>%
-  mutate(arány=value/100,
-         kateg=tolower(kateg),
-         datum=gsub("_","/",k_yr)) %>% 
-  select(!c(value,url)) %>%
-  group_by(tipus) %>% { 
-    set_names(group_split(.,.keep=F), group_keys(.)$tipus) } %>%
-  as.list()
+for (p in periods) {
+  # container
+  l_part_data[["median"]][[p]] <- list()
+  
+  # demographic splits
+  l_part_data[["median"]][[p]] <- load_median_demog(p)
+  
+  # teljes népesség
+  l_part_data[["median"]][[p]]$telj_nepesseg_partok <- load_median_fullpop(p)
+  
+  # 2025_06-specific post-processing
+  if (p == "2025_06") {
+    # rename KORCSOPORT -> ÉLETKOR if present
+    names(l_part_data$median$`2025_06`)[
+      names(l_part_data$median$`2025_06`) %in% "KORCSOPORT"
+    ] <- "ÉLETKOR"
+    
+    # TELEPÜLÉSTÍPUS -> TELEPÜLÉS
+    l_part_data$median$`2025_06`$TELEPÜLÉS <- l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS
+    l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS <- NULL
+  }
 }
-rm(k_yr)
-# Median teljes nepesseg (partokra bontva)
-# https://flo.uri.sh/visualisation/25049245/embed?auto=1
-for (period_name in c("2025_11","2026_01") ) {
 
-l_part_data[["median"]][[period_name]]$telj_nepesseg_partok <- read_csv(
-  paste0("input_files/median_",period_name,"_teljes_nepesseg.csv")) %>% 
-  pivot_longer(!c(kateg,url,datum),names_to="part") %>%
-  filter(grepl("Teljes",kateg)) %>%
-  mutate(kateg=tolower(kateg),arány=value/100) %>%
-  select(!c(value,url)) %>%
-  relocate(c(datum),.after=last_col()) %>%
-  bind_rows(data.frame(kateg="teljes népesség",
-    part="Jobbik",datum=gsub("_","/",period_name))) # arány=0,
-
-}
-rm(period_name)
+rm(p,periods)
   
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
@@ -642,6 +567,7 @@ l_plot$median$partnev_kozos_aggr <- factor(l_plot$median$partnev_kozos_aggr,
 
 # abszolut szamok
 if (F) {
+  
 lapply(unique(l_plot$median$kateg_tipus), \(x_nev)
 bind_rows(
   l_plot$`21_kut` %>% mutate(cég="21kut"), 
@@ -723,6 +649,113 @@ xx <- bind_rows(
         # factor(gsub("/\n","/",partnev_kozos_aggr),
         #               levels = unique(gsub("/\n","/",partnev_kozos_aggr)))
       ) %>%
-  write_csv(file=paste0("output/wide_absz_szam_szazalek/l_plot_",gsub(" ","",x_kat),".csv"))
+  write_csv(file=paste0("output/l_plot_",gsub(" ","",x_kat),".csv"))
   }
 )
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+## dustbin of history
+
+# # MEDIAN 2025/06, tobb demogr kateg
+# # https://datawrapper.dwcdn.net/Ns8XR/10/
+# l_part_data[["median"]][["2025_06"]] = list()
+# l_part_data[["median"]][["2025_06"]] <- read_csv(
+#   "input_files/median_2025_06_nem_vegzettseg_telepules_vegzettseg.csv") %>%
+#   select(!url) %>%
+#   pivot_longer(!c(kateg_tipus,kateg,datum),names_to="part") %>%
+#   mutate(arány=value/100,
+#         part=gsub("\\*","",part),
+#         kateg=tolower(kateg),
+#         kateg=gsub("/vagy idősebb","\\+",gsub(" éves","",gsub(" végzettség","",gsub("\\, ","/",
+#                 kateg))))  ) %>% 
+#   select(!value) %>%
+#   group_by(kateg_tipus) %>% { 
+#     set_names(group_split(.,.keep=F), group_keys(.)$kateg_tipus) } %>%
+#   as.list()
+# 
+# # l_part_data[["median"]][["2025_06"]]$KORCSOPORT
+# # l_part_data[["median"]][["2025_08"]]$ÉLETKOR
+# names(l_part_data[["median"]][["2025_06"]])[
+#   names(l_part_data[["median"]][["2025_06"]]) %in% "KORCSOPORT"] <- "ÉLETKOR"
+# 
+# # teljes nepesseg
+# # datawrapper.dwcdn.net/RLw52/2/
+# l_part_data[["median"]][["2025_06"]]$telj_nepesseg_partok <- read_csv(
+#   "input_files/median_2025_06_teljes_nepesseg.csv") %>% 
+#   rename(part=Párt) %>%
+#   select(part,`teljes népesség`) %>%
+#   pivot_longer(!part,names_to = "kateg") %>%
+#   mutate(arány=value/100,datum="2025/06") %>%
+#   select(!value)
+# 
+# l_part_data$median$`2025_06`$TELEPÜLÉS <- l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS
+# l_part_data$median$`2025_06`$TELEPÜLÉSTÍPUS=NULL
+# 
+# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+# # MEDIAN 2025/08, tobb demogr kateg
+# # source: https://flo.uri.sh/visualisation/25049528/embed?auto=1
+# 
+# l_part_data[["median"]][["2025_08"]] <- list()
+# l_part_data[["median"]][["2025_08"]] <- read_csv(
+#   "input_files/median_2025_08_nem_vegzettseg_telepules_vegzettseg.csv") %>%
+#   rename(tipus=filter) %>%
+#   pivot_longer(!c(tipus,kateg),names_to="part") %>%
+#   mutate(arány=value/100,
+#          kateg=tolower(kateg),
+#          datum="2025/08"  ) %>% 
+#   select(!value) %>%
+#   group_by(tipus) %>% { set_names(group_split(.,.keep=F), group_keys(.)$tipus) } %>%
+#   as.list()
+# 
+# # teljes nepesseg partokra bontva
+# # https://flo.uri.sh/visualisation/25049245/embed?auto=1
+# l_part_data[["median"]][["2025_08"]]$telj_nepesseg_partok <- read_csv(
+#   "input_files/median_2025_08_teljes_nepesseg.csv") %>% 
+#   pivot_longer(!c(kateg,url,datum),names_to="part") %>%
+#   filter(grepl("Teljes",kateg)) %>%
+#   mutate(kateg=tolower(kateg),arány=value/100) %>%
+#   select(!c(value,url)) %>%
+#   relocate(c(datum),.after=last_col()) %>%
+#   bind_rows(data.frame(kateg="teljes népesség",
+#     part="Jobbik",datum="2025/08")) # arány=0,
+# 
+# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+# # MEDIAN 2025/11, tobb demogr kateg
+# # source: https://flo.uri.sh/visualisation/26553099/embed?auto=1
+# 
+# # MEDIAN 2025/11, tobb demogr kateg
+# # source: https://flo.uri.sh/visualisation/26553099/embed?auto=1
+# 
+# for (k_yr in c("2025_11","2026_01")) {
+# l_part_data[["median"]][[k_yr]] <- list()
+# l_part_data[["median"]][[k_yr]] <- read_csv(
+#   paste0("input_files/median_",k_yr,"_nem_vegzettseg_telepules_vegzettseg.csv")) %>%
+#   rename(tipus=filter) %>%
+#   pivot_longer(!c(tipus,kateg,url),names_to="part") %>%
+#   mutate(arány=value/100,
+#          kateg=tolower(kateg),
+#          datum=gsub("_","/",k_yr)) %>% 
+#   select(!c(value,url)) %>%
+#   group_by(tipus) %>% { 
+#     set_names(group_split(.,.keep=F), group_keys(.)$tipus) } %>%
+#   as.list()
+# }
+# rm(k_yr)
+# # Median teljes nepesseg (partokra bontva)
+# # https://flo.uri.sh/visualisation/25049245/embed?auto=1
+# for (period_name in c("2025_11","2026_01") ) {
+# 
+# l_part_data[["median"]][[period_name]]$telj_nepesseg_partok <- read_csv(
+#   paste0("input_files/median_",period_name,"_teljes_nepesseg.csv")) %>% 
+#   pivot_longer(!c(kateg,url,datum),names_to="part") %>%
+#   filter(grepl("Teljes",kateg)) %>%
+#   mutate(kateg=tolower(kateg),arány=value/100) %>%
+#   select(!c(value,url)) %>%
+#   relocate(c(datum),.after=last_col()) %>%
+#   bind_rows(data.frame(kateg="teljes népesség",
+#     part="Jobbik",datum=gsub("_","/",period_name))) # arány=0,
+# 
+# }
+# rm(period_name)
+
